5335f8394bfd98cf1a2ba504f144a51eb8b99897
Parth <parthsata1709@gmail.com>
2025-02-25 20:46:19 +0530

    Add chat-related models

--- chat-app-backend/src/models/message.model.js
+++ chat-app-backend/src/models/message.model.js
@@ -1,27 +1,12 @@
 import mongoose from "mongoose";
 
-const MessageSchema = new mongoose.Schema(
-  {
-    sender: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
-      required: true,
-    }, // Sender ID
-    chatId: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Chat",
-      required: true,
-    }, // Chat ID
+const MessageSchema = new mongoose.Schema({
+    sender: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true }, // Sender ID
+    chatId: { type: mongoose.Schema.Types.ObjectId, ref: "Chat", required: true }, // Chat ID
     content: { type: String, required: true }, // Message text
-    messageType: {
-      type: String,
-      enum: ["text", "image", "file"],
-      default: "text",
-    }, // Message type
+    messageType: { type: String, enum: ["text", "image", "file"], default: "text" }, // Message type
     fileUrl: { type: String, default: "" }, // If messageType is file/image
     seenBy: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }], // Users who have read the message
-  },
-  { timestamps: true }
-);
+}, { timestamps: true });
 
 export const Message = mongoose.model("Message", MessageSchema);
