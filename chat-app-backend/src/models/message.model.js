2f7f66ca9ae97bba9212a12728241956a608abcd
Parth <parthsata1709@gmail.com>
2025-02-25 20:47:14 +0530

    Model created

--- chat-app-backend/src/models/message.model.js
+++ chat-app-backend/src/models/message.model.js
@@ -1,12 +1,11 @@
 import mongoose from "mongoose";
 
 const MessageSchema = new mongoose.Schema({
-    sender: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true }, // Sender ID
-    chatId: { type: mongoose.Schema.Types.ObjectId, ref: "Chat", required: true }, // Chat ID
-    content: { type: String, required: true }, // Message text
-    messageType: { type: String, enum: ["text", "image", "file"], default: "text" }, // Message type
-    fileUrl: { type: String, default: "" }, // If messageType is file/image
-    seenBy: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }], // Users who have read the message
+    recipient: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true }, // User who receives notification
+    sender: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true }, // User who sent the message
+    chatId: { type: mongoose.Schema.Types.ObjectId, ref: "Chat", required: true },
+    message: { type: mongoose.Schema.Types.ObjectId, ref: "Message" }, // Last unread message
+    isRead: { type: Boolean, default: false },
 }, { timestamps: true });
 
 export const Message = mongoose.model("Message", MessageSchema);
